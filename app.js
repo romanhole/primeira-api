a6c4760ecdb08d98f48154872bbc55ede5c8ae92
Romanhole <rafa.rborroz@gmail.com>
2024-05-23 17:13:35 -0300

    create and read files

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
-const http = require('http')
+import http from 'http'
+import fs from 'fs'
 
-const servidor = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-type', 'text/plain; charset=utf-8')
-    res.end('Olá, TIC em Trilhas')
+fs.writeFile('./mensagem.txt', 'Olá TIC em Trilhas do arquivo', 'utf-8', (erro) => {
+    if (erro) {
+        console.log('Falha ao escrever o arquivo', erro)
+        return
+    }
+    console.log('Arquivo criado com sucesso')
 })
 
-const porta = 3000
-const host = 'localhost'
+fs.readFile('./mensagem.txt', 'utf-8', (erro, conteudo) => {
+    if (erro) {
+        console.log('Falha na leitura do arquivo', erro)
+        return
+    }
 
-servidor.listen(porta, host, () => {
-    console.log(`Servidor executando em http://${host}:${porta}/`)
+    console.log(`Conteúdo: ${conteudo}`)
+
+    iniciaServidorHttp(conteudo)
 })
 
-exemploTradicional()
-
-function exemploTradicional() {
-    console.log('Tradicional')
-}
-
-const exemploExpressao = function() {
-    console.log('Expressão')
-}
-
-const exemploArrow = () => {
-    console.log('Arrow')
+function iniciaServidorHttp(mensagem) {
+    const servidor = http.createServer((req, res) => {
+        res.statusCode = 200
+        res.setHeader('Content-type', 'text/plain; charset=utf-8')
+        res.end(mensagem)
+    })
+    
+    const porta = 3000
+    const host = 'localhost'
+    
+    servidor.listen(porta, host, () => {
+        console.log(`Servidor executando em http://${host}:${porta}/`)
+    })
 }
