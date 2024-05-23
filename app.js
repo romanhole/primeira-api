7111bb07596897d1045e217c8c57961dcc959889
Romanhole <rafa.rborroz@gmail.com>
2024-05-23 16:52:54 -0300

    create server

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('Olá, mundo!')
+const http = require('http')
+
+const servidor = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-type', 'text/plain; charset=utf-8')
+    res.end('Olá, TIC em Trilhas')
+})
+
+const porta = 3000
+const host = 'localhost'
+
+servidor.listen(porta, host, () => {
+    console.log(`Servidor executando em http://${host}:${porta}/`)
+})
+
+exemploTradicional()
+
+function exemploTradicional() {
+    console.log('Tradicional')
+}
+
+const exemploExpressao = function() {
+    console.log('Expressão')
+}
+
+const exemploArrow = () => {
+    console.log('Arrow')
+}
