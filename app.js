e46b33addf0fab9c60be541a9dd2d9fdc5645427
Romanhole <rafa.rborroz@gmail.com>
2024-05-30 14:34:16 -0300

    import sqlite3 e criação de um banco de dados

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 import http from 'http'
 import fs from 'fs'
 import rotas from './routes.js'
+import sqlite3 from 'sqlite3'
+
+const db = new sqlite3.Database('./tic.db', (erro) => {
+    if (erro) {
+        console.log('Falha ao inicializar o banco de dados')
+        return
+    }
+    console.log('Conexão com o banco de dados realizada com sucesso')
+})
 
 fs.writeFile('./mensagem.txt', 'Olá TIC em Trilhas do arquivo', 'utf-8', (erro) => {
     if (erro) {
