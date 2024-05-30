f37ae1bf4ba89eb8323a83de6b3641d9df72e253
Romanhole <rafa.rborroz@gmail.com>
2024-05-30 15:00:52 -0300

    import sequelize and sync connect with db

--- app.js
+++ app.js
@@ -2,13 +2,27 @@ import http from 'http'
 import fs from 'fs'
 import rotas from './routes.js'
 import sqlite3 from 'sqlite3'
+import { Sequelize } from 'sequelize'
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './tic.db'
+})
+
+sequelize.authenticate()
+    .then(() => {
+        console.log('Conexão com o banco de dados realizada com sucesso')
+    })
+    .catch((erro) => {
+        console.log('Falha ao conectar com o banco de dados', erro)
+    })
 
 const db = new sqlite3.Database('./tic.db', (erro) => {
     if (erro) {
         console.log('Falha ao inicializar o banco de dados')
         return
     }
-    console.log('Conexão com o banco de dados realizada com sucesso')
+    console.log('Banco de dados inicialixado com sucesso')
 })
 
 fs.writeFile('./mensagem.txt', 'Olá TIC em Trilhas do arquivo', 'utf-8', (erro) => {
